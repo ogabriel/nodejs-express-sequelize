4ce44aa351af30c7265e5792b9ce4ec7354d3e5f
ogabriel <gabrieloliver8991@gmail.com>
2020-04-01 23:32:27 -0300

    fix database configuration and add sample of envs

--- config/config.js
+++ config/config.js
@@ -1,32 +1,16 @@
+require('dotenv').config()
+
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
-    logging: false,
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
-    logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
   },
-};
+}

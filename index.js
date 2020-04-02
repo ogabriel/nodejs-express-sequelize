a077d8adc2eeb278ade52dd50cd0139e3225cbfc
ogabriel <gabrieloliver8991@gmail.com>
2020-04-02 00:13:17 -0300

    call usre

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const { User } = require('./models');
+User.create({ name: 'Claudio', email: 'claudio@rocketseat.com.br', password: '123456' });
+
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
